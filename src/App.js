da5466416bc314dd381fd26a803339ba04e703b6
Tỷ Nguyễn <tynguyen@Tys-MacBook-Pro.local>
2023-06-27 09:19:06 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+// import Image from "next/image";
+// import { Inter } from "next/font/google";
+import { ethers } from "ethers";
+import ATM from "./ATM.json";
+import { useState } from "react";
+// const inter = Inter({ subsets: ["latin"] });
+export default function Home() {
+  const [addressUser, setAddressUser] = useState(undefined);
+  const [amount, setamount] = useState(undefined);
+  const [setsingerr, setSetsingerr] = useState(undefined);
+  const handleTransfer = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(
+        "0x2618267931f2078fD3fef4b817306DA8Da3ff107",
+        ATM,
+        signer
+      );
+      // const transaction = await contract.transfer(addressUser, amount);
+      const transaction = await contract.balanceOf('0x2038E077C5fB0E0bDe287cfEA203Db516af61643');
+      // const transactionFrom = await contract.transferFrom('0x2038E077C5fB0E0bDe287cfEA203Db516af61643' ,addressUser, amount);
+      // Chờ giao dịch được xác nhận
+      // await transaction.wait();
+      // const formartBalanceToken = convertToEther(valueprice, 4);
+      console.log(ethers.utils.formatEther( transaction ))
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-black">
+      <div>
+        <input
+          onChange={(e) => setAddressUser(e.target.value)}
+          placeholder="adddress"
+        />
+        <input
+          onChange={(e) => setamount(e.target.value)}
+          placeholder="Amount"
+        />
+
+        <button className="text-white" onClick={() => handleTransfer()}>
+          Transfer
+        </button>
+      </div>
     </div>
   );
 }
-
-export default App;
